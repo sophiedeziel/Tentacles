e47f3cd8b6344fb6a1076c00d0268953c1a67865
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-06 00:39:51 -0500

    Prototype to connect to multiple printers

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -1,2 +1,7 @@
 class Printer < ApplicationRecord
+  def octoprint_version
+    client = Octoprint::Client.new(host: octoprint_uri, api_key: octoprint_key)
+
+    Octoprint::ServerVersion.get(client: client)
+  end
 end
