5d9c6a40f883a0db737029abb346e2426a29658f
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-06 01:02:43 -0500

    Create the PrinterType

---
+++ app/graphql/types/printer_type.rb
@@ -0,0 +1,14 @@
+module Types
+  class PrinterType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :octoprint_uri, String, null: true
+    field :octoprint_key, String, null: true
+    field :octoprint_version, String, null: true
+    def octoprint_version
+      object.octoprint_version.text
+    end
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,10 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :printers, [PrinterType], null: false,
+      description: "asdf"
+    def printers
+      Printer.all
     end
   end
 end
