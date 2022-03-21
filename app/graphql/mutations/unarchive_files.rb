1b360f4801af51ad78ea59974026f0e9b99e13bf
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-20 23:27:21 -0400

    Unarchive

---
+++ app/graphql/mutations/unarchive_files.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UnarchiveFiles < Mutations::BaseMutation
+    description 'Mutation to unarchive multiple files'
+
+    argument :file_ids,
+             [ID],
+             required: true
+
+    field :success, Boolean, null: false
+
+    def resolve(file_ids:)
+      files = FileManager::File.where(id: file_ids)
+
+      files.each(&:unarchive)
+      { success: true }
+    rescue StandardError
+      { success: false }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,8 @@
 
 module Types
   class MutationType < Types::BaseObject
-    field :uploadFile, mutation: Mutations::UploadFile
     field :archiveFiles, mutation: Mutations::ArchiveFiles
+    field :unarchiveFiles, mutation: Mutations::UnarchiveFiles
+    field :uploadFile, mutation: Mutations::UploadFile
   end
 end
