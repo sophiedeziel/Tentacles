24110a41b76c44ab82c62ad5138be319707d7a44
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-23 16:54:36 -0400

    try monaco editor

--- app/ui/Sections/Files/FileEditor/FileEditor.jsx
+++ app/ui/Sections/Files/FileEditor/FileEditor.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
 import { useRouteMatch } from 'react-router'
-import CodeMirror from '@uiw/react-codemirror'
-import { GCodeLanguage } from 'gcode-lang-codemirror'
-
+import Editor from '@monaco-editor/react'
 import { useQuery } from '@apollo/client'
 import File from './graphql/File.graphql'
 
 export default function FileEditor () {
   const match = useRouteMatch('/files/:id')
   const fileID = match.params.id
-  const [code, setCode] = useState()
   const { loading, error, data: fileData } = useQuery(File, {
     variables: { id: fileID }
   })
@@ -20,13 +17,20 @@ export default function FileEditor () {
 
   const { file } = fileData
 
+  function handleEditorMount (editor) {
+    editor.onDidChangeCursorPosition(console.log)
+  }
+
   return (
     <>
-      <CodeMirror
-      value={file.fileContent}
-      extensions={[GCodeLanguage()]}
-      height="calc(100vh - 190px)"
-    />
+
+      <Editor
+        height="90vh"
+        defaultLanguage="gcode"
+        path={file.filename}
+        defaultValue={file.fileContent}
+        onMount={handleEditorMount}
+      />
     </>
   )
 }
