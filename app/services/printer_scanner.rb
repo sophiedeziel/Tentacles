c2ec409f7780e19a6449d7c32197a814de096535
Sophie Déziel <courrier@sophiedeziel.com>
2022-04-21 19:05:21 -0400

    scan improvements

--- app/services/printer_scanner.rb
+++ app/services/printer_scanner.rb
@@ -12,7 +12,9 @@ class PrinterScanner
   private
 
   def all_network_ips
-    network_scan = `nmap -sn 10.0.1.0/24`
+    return [] unless local_ip
+
+    network_scan = `nmap -sP #{local_ip}/24`
     network_scan.split("\n").map { |line| IP_REGEX.match(line) }.compact.map { |line| line[1] }
   end
 
@@ -24,5 +26,9 @@ class PrinterScanner
     rescue StandardError
       false
     end
+  end
+
+  def local_ip
+    Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
   end
 end
