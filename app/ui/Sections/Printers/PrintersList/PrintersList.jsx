ca8c90ad1b9a9a2d3462d718c22da00d4b6d5121
Sophie Déziel <courrier@sophiedeziel.com>
2022-04-21 21:07:52 -0400

    Show that printers were found

--- app/ui/Sections/Printers/PrintersList/PrintersList.jsx
+++ app/ui/Sections/Printers/PrintersList/PrintersList.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 import { useQuery } from '@apollo/client'
+import { Empty } from 'antd'
+
 import Printers from './graphql/Printers.graphql'
+import NetworkPrinters from './NetworkPrinters'
 
 export default function PrintersList () {
   const { loading, error, data: printersData } = useQuery(Printers)
@@ -12,18 +15,30 @@ export default function PrintersList () {
 
   const { printers } = printersData
 
+  const DisplayPrintersList = () => {
+    if (printers.length === 0) {
+      return <Empty
+        description="Configure a printer to see it here"
+      />
+    }
+    return (
+      <ul>{
+        printers.map((printer) => {
+          return (
+          <li key={printer.id}>
+            {printer.name}, {printer.octoprintVersion}
+          </li>
+          )
+        })
+        }
+      </ul>
+    )
+  }
+
   return (
     <>
-      <ul>{
-      printers.map((printer) => {
-        return (
-        <li key={printer.id}>
-          {printer.name}, {printer.octoprintVersion}
-        </li>
-        )
-      })
-      }
-      </ul>
+      <NetworkPrinters />
+      <DisplayPrintersList />
     </>
   )
 }
