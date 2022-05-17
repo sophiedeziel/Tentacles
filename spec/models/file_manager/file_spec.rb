90df1c903305a8428b418725f80f3c03c1bf3689
Sophie Déziel <courrier@sophiedeziel.com>
2022-05-16 20:53:50 -0400

    complete file tests

--- spec/models/file_manager/file_spec.rb
+++ spec/models/file_manager/file_spec.rb
@@ -8,4 +8,77 @@ RSpec.describe FileManager::File, type: :model do
   it 'has a valid factory' do
     expect(file).to be_valid
   end
+
+  describe '#filename' do
+    subject { file.filename }
+
+    it { is_expected.to eq 'test.gcode' }
+  end
+
+  describe '#filetype' do
+    subject { file.filetype }
+
+    it { is_expected.to eq '.gcode' }
+  end
+
+  describe '#filesize' do
+    subject { file.filesize }
+
+    it { is_expected.to eq 949_599 }
+  end
+
+  describe '#archive' do
+    subject { file.archive }
+
+    it 'archives the file' do
+      expect { subject }.to change { file.is_not_archived }.from(true).to(false)
+    end
+  end
+
+  describe '#unarchive' do
+    subject { file.unarchive }
+
+    it 'unarchives the file' do
+      file.archive
+      expect { subject }.to change { file.is_not_archived }.from(false).to(true)
+    end
+  end
+
+  describe '#top_file_comments' do
+    subject { file.top_file_comments }
+
+    it 'returns the comment lines from the top of the file' do
+      expect(subject).to eq [
+        ';FLAVOR:Marlin',
+        ';TIME:4271',
+        ';Filament used: 0.797727m',
+        ';Layer height: 0.12',
+        ';MINX:119.5',
+        ';MINY:39.5',
+        ';MINZ:0.12',
+        ';MAXX:180.5',
+        ';MAXY:260.5',
+        ';MAXZ:0.36',
+        ';Generated with Cura_SteamEngine 4.11.0'
+      ].join("\r\n")
+    end
+  end
+
+  describe '#file_content' do
+    subject { file.file_content }
+
+    it { is_expected.to eq File.read(Rails.root.join('spec/fixture_files/test.gcode')) }
+  end
+
+  describe '#change_file_content!' do
+    subject { file.change_file_content!(content) }
+
+    let(:content) { 'new_content' }
+
+    it 'changes the file content' do
+      expect { subject }.to change {
+                              file.file_content
+                            }.from(File.read(Rails.root.join('spec/fixture_files/test.gcode'))).to(content)
+    end
+  end
 end
