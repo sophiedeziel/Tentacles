75c25c1eba9fe02718813edacdff4cc367fe6bdb
Sophie Déziel <courrier@sophiedeziel.com>
2022-09-26 19:29:50 -0400

    rubocop -A

--- spec/factories/file_manager/files.rb
+++ spec/factories/file_manager/files.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :file_manager_file, class: 'FileManager::File' do
     after(:build) do |post|
       post.file.attach(
-        io: File.open(Rails.root.join('spec/fixture_files/test.gcode')),
+        io: Rails.root.join('spec/fixture_files/test.gcode').open,
         filename: 'test.gcode',
         content_type: 'application/octet-stream'
       )
@@ -13,7 +13,7 @@ FactoryBot.define do
     trait :only_comments do
       after(:build) do |post|
         post.file.attach(
-          io: File.open(Rails.root.join('spec/fixture_files/only_comments.gcode')),
+          io: Rails.root.join('spec/fixture_files/only_comments.gcode').open,
           filename: 'test.gcode',
           content_type: 'application/octet-stream'
         )
