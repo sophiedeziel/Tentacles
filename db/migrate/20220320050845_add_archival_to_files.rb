bccaafad41c8996aa2fb456dd127ffc9a47be0e8
Sophie Déziel <courrier@sophiedeziel.com>
2023-05-15 11:36:46 -0400

    Adding a not null constraint on the boolean column

--- db/migrate/20220320050845_add_archival_to_files.rb
+++ db/migrate/20220320050845_add_archival_to_files.rb
@@ -3,7 +3,7 @@
 class AddArchivalToFiles < ActiveRecord::Migration[7.0]
   def change
     change_table :files, bulk: true do |t|
-      t.boolean :is_not_archived, default: true
+      t.boolean :is_not_archived, default: true, null: false
       t.datetime :archived_at, default: nil
     end
   end
