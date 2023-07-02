95498218331ab963cf41bf7435544850729ce328
Sophie Déziel <courrier@sophiedeziel.com>
2023-07-01 22:55:51 -0400

    Remove action mailer config and tests

--- spec/mailers/application_spec.rb
+++ spec/mailers/application_spec.rb
@@ -3,16 +3,16 @@
 require 'rails_helper'
 
 # rubocop:disable Rails/I18nLocaleTexts
-class TestMailer < ApplicationMailer
-  def test_email
-    mail(to: 'test@test.com', subject: 'Test Email', content_type: 'text/html', body: 'This is a test email')
-  end
-end
-# rubocop:enable Rails/I18nLocaleTexts
+# class TestMailer < ApplicationMailer
+#   def test_email
+#     mail(to: 'test@test.com', subject: 'Test Email', content_type: 'text/html', body: 'This is a test email')
+#   end
+# end
+# # rubocop:enable Rails/I18nLocaleTexts
 
-RSpec.describe ApplicationMailer, type: :mailer do
-  it 'defaults from' do
-    mail = TestMailer.test_email
-    expect(mail.from).to eq(['courrier@sophiedeziel.com'])
-  end
-end
+# RSpec.describe ApplicationMailer, type: :mailer do
+#   it 'defaults from' do
+#     mail = TestMailer.test_email
+#     expect(mail.from).to eq(['courrier@sophiedeziel.com'])
+#   end
+# end
