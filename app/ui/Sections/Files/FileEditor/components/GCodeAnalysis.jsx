848f0a3714bed89266ae623c62f0d28b5d073692
Sophie Déziel <courrier@sophiedeziel.com>
2023-11-27 17:14:36 -0500

    Display in tables

--- app/ui/Sections/Files/FileEditor/components/GCodeAnalysis.jsx
+++ app/ui/Sections/Files/FileEditor/components/GCodeAnalysis.jsx
@@ -3,20 +3,93 @@ import PropTypes from 'prop-types'
 import { Collapse, Table } from 'antd'
 
 function GCodeAnalysis ({ gcodeAnalysis, onLineSelect }) {
+  const layersTable = () => (
+    <Table
+    size="small"
+    columns={[
+      {
+        title: 'Layer',
+        dataIndex: 'id',
+        key: 'id'
+      },
+      {
+        title: 'Line',
+        dataIndex: 'lineNumber',
+        key: 'lineNumber',
+        render: (lineNumber) => (
+          <pre onClick={() => { onLineSelect(lineNumber) }}><a>{lineNumber}</a></pre>
+        )
+      },
+      {
+        title: 'Height',
+        dataIndex: 'height',
+        key: 'height'
+      },
+      {
+        title: 'Z Height',
+        dataIndex: 'z',
+        key: 'z'
+      }
+    ]}
+    dataSource={gcodeAnalysis.layers}
+  />
+  )
+
+  const bedTemperaturesTable = () => (
+    <Table
+    size="small"
+    columns={[
+      {
+        title: 'Line',
+        dataIndex: 'lineNumber',
+        key: 'lineNumber',
+        render: (lineNumber) => (
+          <pre onClick={() => { onLineSelect(lineNumber) }}><a>{lineNumber}</a></pre>
+        )
+      },
+      {
+        title: 'Temperature',
+        dataIndex: 'temperature',
+        key: 'temperature'
+      }
+    ]}
+    dataSource={gcodeAnalysis.bedTemperatures}
+  />
+  )
+
+  const hotendTemperaturesTable = () => (
+    <Table
+    size="small"
+    columns={[
+      {
+        title: 'Line',
+        dataIndex: 'lineNumber',
+        key: 'lineNumber',
+        render: (lineNumber) => (
+          <pre onClick={() => { onLineSelect(lineNumber) }}><a>{lineNumber}</a></pre>
+        )
+      },
+      {
+        title: 'Temperature',
+        dataIndex: 'temperature',
+        key: 'temperature'
+      }
+    ]}
+    dataSource={gcodeAnalysis.hotendTemperatures}
+  />
+  )
+
   const items = [
     {
       key: '1',
       label: 'Hotend Temperatures',
-      children:
-      gcodeAnalysis.hotendTemperatures.map((temp) => (
-        <pre onClick={() => { onLineSelect(temp.lineNumber) }} key={temp.lineNumber}>Line {temp.lineNumber}: {temp.temperature}</pre>
-      ))
+      children: hotendTemperaturesTable()
 
     },
     {
       key: '2',
       label: 'Bed Temperatures',
-      children: <p></p>
+      children: bedTemperaturesTable()
     },
     {
       key: '3',
@@ -25,39 +98,7 @@ function GCodeAnalysis ({ gcodeAnalysis, onLineSelect }) {
     }, {
       key: '4',
       label: 'Layers',
-      children:
-      <Table
-        size="small"
-        columns={[
-          {
-            title: 'Layer',
-            dataIndex: 'id',
-            key: 'id',
-          },
-          {
-            title: 'Line',
-            dataIndex: 'lineNumber',
-            key: 'lineNumber',
-            render: (lineNumber) => (
-              <pre onClick={() => { onLineSelect(lineNumber) }}><a>{lineNumber}</a></pre>
-            )
-          },
-          {
-            title: 'Height',
-            dataIndex: 'height',
-            key: 'height',
-          },
-          {
-            title: 'Z Height',
-            dataIndex: 'z',
-            key: 'z',
-          }
-        ]}
-        dataSource={gcodeAnalysis.layers}
-      />
-      // gcodeAnalysis.layers.map((layer) => (
-      //   <pre onClick={() => { onLineSelect(layer.lineNumber) }} key={layer.id}> {layer.id} Line {layer.lineNumber}: layer height: {layer.height} height: {layer.zHeight}</pre>
-      // ))
+      children: layersTable()
     }
   ]
 
