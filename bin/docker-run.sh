cfdbbcfb3c88f38ad9763783a1698ac175849330
Sophie Déziel <courrier@sophiedeziel.com>
2024-06-23 02:15:58 -0400

    prefix to run migrations successfully

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -4,5 +4,5 @@ export SECRET_KEY_BASE=`bundle exec rails secret`
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
-rails db:create db:migrate
+bin/rails db:create db:migrate
 foreman start -f Procfile.prod
